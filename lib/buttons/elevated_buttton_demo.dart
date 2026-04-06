a1b80f5b40cbd284d6dd593ef3819d3933e07881
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-04-06 11:25:39 +0530

    feat(elevated-button): add elevated button with required parameters

---
+++ lib/buttons/elevated_buttton_demo.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class ElevatedButttonDemo extends StatelessWidget {
+  const ElevatedButttonDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        shadowColor: Colors.green,
+        padding: const EdgeInsets.only(
+          top: 15,
+          bottom: 15,
+          right: 20,
+          left: 20,
+        ),
+        elevation: 4,  // to add shadow ncliment 
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),  // to add border radius to the button 
+        ),
+        side: const BorderSide( // to add border to the button
+          color: Colors.red,
+          width: 1,
+        ),
+      ),
+
+      onPressed: () {
+        debugPrint('Button Clicked !!!');
+      },
+
+      child: const Text(
+        'Click',
+        style: TextStyle(
+          fontSize: 35
+        ),
+      ),
+
+    );
+  }
+
+}
