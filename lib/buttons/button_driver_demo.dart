ff2cb1f11b469aecee3cf2dcc8780b8acd079602
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-04-06 11:23:05 +0530

    feat(button-driver): add driver program to execuuted button widgit

---
+++ lib/buttons/button_driver_demo.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_basics/buttons/elevated_buttton_demo.dart';
+
+class ButtonDriverDemo extends StatelessWidget {
+  const ButtonDriverDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      // app bar
+      appBar: AppBar(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.black,
+        title: const Text(
+          'Buttons',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+
+        // action buttons for app bar icons
+        actions: const [
+          Icon(
+            Icons.location_city,
+          ),
+        ],
+      ),
+
+      // drawer
+      drawer: const Icon(
+        Icons.grid_4x4,
+      ),
+
+      // body
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(10),
+        decoration:
+            const BoxDecoration(color: Color.fromARGB(255, 228, 226, 226)),
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // elevated button
+            ElevatedButttonDemo()
+          ],
+        ),
+      ),
+
+      // bottom nav bar
+
+    );
+  }
+}
