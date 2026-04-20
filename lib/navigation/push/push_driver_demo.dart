e3dcd9bb59888b8303ba3f97613e2fca73cc6c88
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-04-20 23:21:15 +0530

    feat(push): add driver program for navigation through push

---
+++ lib/navigation/push/push_driver_demo.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_basics/navigation/push/first_page.dart';
+
+class PushDriverDemo extends StatelessWidget {
+  const PushDriverDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const FirstPage();
+  }
+}
