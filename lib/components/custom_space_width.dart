ecacfb5926007d54bc59be1f4903c06ea8d8ab2e
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-05-03 08:59:08 +0530

    feat(space-comp): add componenets for fixed height and width

---
+++ lib/components/custom_space_width.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CustomSpaceWidth extends StatelessWidget {
+  const CustomSpaceWidth({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: 15,
+    );
+  }
+}
